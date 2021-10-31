cf028a65aa8e96e67e79f389eead1970c1346ce1
Gaussito <cm.andres022@hotmail.com>
2021-10-31 17:38:31 -0500

    Errores Solucionados: Modificando grupos

--- settings/groups.py
+++ settings/groups.py
@@ -5,7 +5,7 @@ from .keys import mod, keys
 
 
 groups = [Group(i) for i in [
-    "   ", "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ",
+    "   ", "   ", "   "
 ]]
 
 for i, group in enumerate(groups):
